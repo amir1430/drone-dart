a8bd4e3edbce1b72d9a0a5e742b53f534ebd6da6
amir1430 <amirhossein13791430@gmail.com>
2022-04-05 21:58:32 +0430

    edit template_request_body data class

--- lib/src/models/template_request_body_model.dart
+++ lib/src/models/template_request_body_model.dart
@@ -1,18 +1,30 @@
-class TemplateRequestBody {
+class Template {
+  factory Template.fromJson(Map<String, dynamic> json) => Template(
+        id: json['id'] ?? 1,
+        name: json['name'] ?? '',
+        data: json['data'] ?? '',
+        namespace: json['namespace'] ?? '',
+      );
+
   // create consutrctor for TemplateRequestBody
-  const TemplateRequestBody({
+  const Template({
+    this.id,
     this.name,
     this.data,
     this.namespace,
   });
 
+  final int? id;
   final String? name;
   final String? data;
   final String? namespace;
 
+// create a fromJson method
+
   // create toJson method
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'name': name,
       'data': data,
       'namespace': namespace,
