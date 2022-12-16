6dc82fb230a50c24e35988f2e7528d1d808d3dfe
amir1430 <amirhossein13791430@gmail.com>
2022-12-16 13:39:04 +0330

    refactor: export [DroneEvent]

--- lib/drone_dart.dart
+++ lib/drone_dart.dart
@@ -1,5 +1,6 @@
 library drone_dart;
 
+export './src/utils/event_to_repo_transformer.dart' show DroneEvent;
 export 'src/drone_dart_base.dart';
 export 'src/exception/drone_exception.dart';
 export 'src/models/models.dart';
