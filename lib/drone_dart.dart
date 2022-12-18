31ca04975ab27c718cf68d54d830b7597a87e7f8
amir1430 <amirhossein13791430@gmail.com>
2022-12-18 14:20:28 +0330

    fix: export [DroneLogEvent]

--- lib/drone_dart.dart
+++ lib/drone_dart.dart
@@ -1,6 +1,7 @@
 library drone_dart;
 
-export './src/utils/event_to_repo_transformer.dart' show DroneEvent;
+export './src/utils/event_to_repo_transformer.dart'
+    show DroneEvent, DroneLogEvent;
 export 'src/drone_dart_base.dart';
 export 'src/exception/drone_exception.dart';
 export 'src/models/models.dart';
